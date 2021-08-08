0bdc4434542f8c4745e9af2b0d852868e5b5ff23
mingyi1992 <charles81914@gmail.com>
2021-08-08 07:23:12 +0000

    coffeev2

--- application.py
+++ application.py
@@ -36,27 +36,24 @@ def callback():
 # message 可以針對收到的訊息種類
 @HANDLER.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    url_dict = {
-      "GOOGLE":"https://www.google.com.tw/?hl=zh_TW", 
-      "HELP":"https://developers.line.biz/zh-hant/docs/messaging-api/"}
+    if event.message.text.upper()=='COFFEE':
+        with open("./coffee.json", "r") as f_r:
+            bubble = json.load(f_r)
+        f_r.close()
+    # 依情況更動 components
+        LINE_BOT.reply_message(
+                event.reply_token,
+            [FlexSendMessage(alt_text="Report", contents=bubble)]
+        )   
+    else:
+        url_dict{
+            "GOOGLE":"https://www.google.com.tw/?hl=zh_TW",
+            "HELP":"https://developers.line.biz/zh-hant/docs/messaging-api/"}
 # 將要發出去的文字變成TextSendMessage
-    try:
-        url = url_dict[event.message.text.upper()]
-        message = TextSendMessage(text=url)
-    except:
-        message = TextSendMessage(text=event.message.text)
+        try:
+            url = url_dict[event.message.text.upper()]
+            message = TextSendMessage(text=url)
+        except:
+            message = TextSendMessage(text=event.message.text)
 # 回覆訊息
     LINE_BOT.reply_message(event.reply_token, message)
-
-with open("./coffee.json", "r") as f_r:
-   bubble = json.load(f_r)
-f_r.close()
-# 依情況更動 components
-bubble["body"]["contents"][0]["contents"][0]["text"] = output
-bubble["header"]["contents"][0]["contents"][0]["url"] = link
-LINE_BOT.reply_message(
-   event.reply_token, 
-   [
-     FlexSendMessage(alt_text="Report", contents=bubble)
-     ]
-)
