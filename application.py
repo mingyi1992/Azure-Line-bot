e0acc1e131d21184ec916fd0a11a2f907758d0ef
mingyi1992 <charles81914@gmail.com>
2021-08-14 06:36:23 +0000

    import items3

--- application.py
+++ application.py
@@ -23,9 +23,8 @@ from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
-from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
-from msrest.authentication import CognitiveServicesCredentials
-
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes,VisualFeatureTypes
+from array import array
 
 app = Flask(__name__)
 
